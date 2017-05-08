d1f5e410f6f6c773e69c17749e7d6694da9c6c19
Nick Kasten <kastencode@gmail.com>
2017-05-07 19:52:59 -0500

    finished tagged template workshopper

--- taggedTemplate.js
+++ taggedTemplate.js
@@ -1,5 +1,21 @@
-console.log(html`<b>${process.argv[2]} says<\b>: "${process.argv[3]}"`)
+console.log(html`<b>${process.argv[2]} says</b>: "${process.argv[3]}"`)
 
-function html(string) {
-  return string
+function html(templateString, ...substitutions) {
+  let result = ""
+  for (let e in substitutions) {
+    var cleanSub = substitutions[e].replace(/&/g, "&amp;")
+    cleanSub = cleanSub.replace(/'/g, "&apos;")
+    cleanSub = cleanSub.replace(/"/g, "&quot;")
+    cleanSub = cleanSub.replace(/</g, "&lt;")
+    cleanSub = cleanSub.replace(/>/g, "&gt;")
+    substitutions[e] = cleanSub
+  }
+
+  for (let i = 0; i < substitutions.length; i++) {
+    result += templateString[i]
+    result += substitutions[i]
+  }
+  result += templateString[templateString.length - 1]
+  
+  return result
 }
